37d3501b5e25d7c6db231560ca052acba84aa88e
Eric Hurst <minkusanderic@gmail.com>
2014-01-25 11:21:07 -0700

    added simple player controller (Dead/Alive)

---
+++ CuriosityCrushSaga/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float jump_strength = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var value = Input.GetAxis("Horizontal");
+		//var a = GetComponent(Rigidbody);
+		var body = gameObject.GetComponent<Rigidbody2D>();
+		//body.AddForce(Vector2.right * value); 
+		body.velocity = new Vector2(value * speed, body.velocity.y);
+		//body.velocity += Vector2.right * value * speed;
+
+		if( Input.GetButtonDown("Jump") )
+		{
+			body.velocity = new Vector2(body.velocity.x, jump_strength);
+		}
+	}
+}
