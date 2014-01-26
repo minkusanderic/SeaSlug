92f34d1e6786d200d33e9d8a23213662fab2686d
Paige Ashlynn <paige.thrush.ashlynn@gmail.com>
2014-01-25 19:08:14 -0700

    Adds temp animation to slugcat.

---
+++ CuriosityCrushSaga/Assets/Scripts/StayByMom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>Locks this gameobject's position to given gameobject's position.</summary>
+public class StayByMom : MonoBehaviour
+{
+    /// <summary>Game object to stick by.</summary>
+    public GameObject mom;
+
+    /// <summary>Move me where I need to be.</summary>
+	void Update ()
+    {
+        if (mom != null)
+        {
+            transform.position = mom.transform.position;
+            transform.rotation = Quaternion.identity;
+        }
+	}
+}
